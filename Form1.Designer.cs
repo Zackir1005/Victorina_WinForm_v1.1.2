90b610f9a6d3aa1810cfdb0c56fc6995a176ea5f
С - 1 < - 1@DESKTOP-C3171P9>
2023-02-06 20:40:14 +0500

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,320 @@
+﻿namespace Victorina_WinForm_v1._1._2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.RTB = new System.Windows.Forms.RichTextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.comboBox5 = new System.Windows.Forms.ComboBox();
+            this.comboBox6 = new System.Windows.Forms.ComboBox();
+            this.comboBox7 = new System.Windows.Forms.ComboBox();
+            this.btnFinish = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.txtBoxUserName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.btnAcceptUserName = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnResult = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // RTB
+            // 
+            this.RTB.Enabled = false;
+            this.RTB.Location = new System.Drawing.Point(2, 1);
+            this.RTB.Name = "RTB";
+            this.RTB.Size = new System.Drawing.Size(372, 448);
+            this.RTB.TabIndex = 0;
+            this.RTB.Text = "";
+            this.RTB.Click += new System.EventHandler(this.RTB_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Enabled = false;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "a",
+            "b",
+            "c"});
+            this.comboBox1.Location = new System.Drawing.Point(400, 10);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.Text = " ";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.Enabled = false;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "a",
+            "b",
+            "c"});
+            this.comboBox2.Location = new System.Drawing.Point(400, 54);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 2;
+            this.comboBox2.Text = " ";
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.Enabled = false;
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "a",
+            "b",
+            "c"});
+            this.comboBox3.Location = new System.Drawing.Point(400, 108);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(121, 21);
+            this.comboBox3.TabIndex = 3;
+            this.comboBox3.Text = " ";
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.Enabled = false;
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Items.AddRange(new object[] {
+            "a",
+            "b",
+            "c"});
+            this.comboBox4.Location = new System.Drawing.Point(400, 156);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(121, 21);
+            this.comboBox4.TabIndex = 4;
+            this.comboBox4.Text = " ";
+            // 
+            // comboBox5
+            // 
+            this.comboBox5.Enabled = false;
+            this.comboBox5.FormattingEnabled = true;
+            this.comboBox5.Items.AddRange(new object[] {
+            "a",
+            "b",
+            "c"});
+            this.comboBox5.Location = new System.Drawing.Point(400, 210);
+            this.comboBox5.Name = "comboBox5";
+            this.comboBox5.Size = new System.Drawing.Size(121, 21);
+            this.comboBox5.TabIndex = 5;
+            this.comboBox5.Text = " ";
+            // 
+            // comboBox6
+            // 
+            this.comboBox6.Enabled = false;
+            this.comboBox6.FormattingEnabled = true;
+            this.comboBox6.Items.AddRange(new object[] {
+            "a",
+            "b",
+            "c"});
+            this.comboBox6.Location = new System.Drawing.Point(400, 296);
+            this.comboBox6.Name = "comboBox6";
+            this.comboBox6.Size = new System.Drawing.Size(121, 21);
+            this.comboBox6.TabIndex = 6;
+            this.comboBox6.Text = " ";
+            // 
+            // comboBox7
+            // 
+            this.comboBox7.Enabled = false;
+            this.comboBox7.FormattingEnabled = true;
+            this.comboBox7.Items.AddRange(new object[] {
+            "a",
+            "b",
+            "c"});
+            this.comboBox7.Location = new System.Drawing.Point(400, 348);
+            this.comboBox7.Name = "comboBox7";
+            this.comboBox7.Size = new System.Drawing.Size(121, 21);
+            this.comboBox7.TabIndex = 7;
+            this.comboBox7.Text = " ";
+            // 
+            // btnFinish
+            // 
+            this.btnFinish.Enabled = false;
+            this.btnFinish.Location = new System.Drawing.Point(700, 400);
+            this.btnFinish.Name = "btnFinish";
+            this.btnFinish.Size = new System.Drawing.Size(80, 24);
+            this.btnFinish.TabIndex = 8;
+            this.btnFinish.Text = "Закончить тест";
+            this.btnFinish.UseVisualStyleBackColor = true;
+            this.btnFinish.Click += new System.EventHandler(this.btnFinish_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Enabled = false;
+            this.btnSave.Location = new System.Drawing.Point(400, 400);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 25);
+            this.btnSave.TabIndex = 9;
+            this.btnSave.Text = "Сохранить";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // txtBoxUserName
+            // 
+            this.txtBoxUserName.Enabled = false;
+            this.txtBoxUserName.Location = new System.Drawing.Point(540, 55);
+            this.txtBoxUserName.Name = "txtBoxUserName";
+            this.txtBoxUserName.Size = new System.Drawing.Size(250, 20);
+            this.txtBoxUserName.TabIndex = 10;
+            this.txtBoxUserName.Text = " ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.OrangeRed;
+            this.label1.Location = new System.Drawing.Point(480, 84);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(293, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Введите имя пользователья и нажмите на экран слева!";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(540, 10);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(80, 24);
+            this.btnStart.TabIndex = 12;
+            this.btnStart.Text = "Начать тест";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoSize = true;
+            this.labelResult.BackColor = System.Drawing.SystemColors.Window;
+            this.labelResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelResult.Location = new System.Drawing.Point(540, 348);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(256, 45);
+            this.labelResult.TabIndex = 13;
+            this.labelResult.Text = "Результаты пользователя :                             \r\nПравильных ответов - \r\nВа" +
+    "ша оценка - ";
+            // 
+            // btnAcceptUserName
+            // 
+            this.btnAcceptUserName.Enabled = false;
+            this.btnAcceptUserName.Location = new System.Drawing.Point(540, 108);
+            this.btnAcceptUserName.Name = "btnAcceptUserName";
+            this.btnAcceptUserName.Size = new System.Drawing.Size(160, 24);
+            this.btnAcceptUserName.TabIndex = 14;
+            this.btnAcceptUserName.Text = "Принять имя пользователя";
+            this.btnAcceptUserName.UseVisualStyleBackColor = true;
+            this.btnAcceptUserName.Click += new System.EventHandler(this.btnAcceptUserName_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.Window;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(540, 156);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(212, 80);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Пожалуйста, введите или \r\nвыберите только вариант \r\na, b, c , иначе ответ не \r\nсч" +
+    "итается!";
+            // 
+            // btnResult
+            // 
+            this.btnResult.Enabled = false;
+            this.btnResult.Location = new System.Drawing.Point(540, 296);
+            this.btnResult.Name = "btnResult";
+            this.btnResult.Size = new System.Drawing.Size(75, 23);
+            this.btnResult.TabIndex = 16;
+            this.btnResult.Text = "Результаты";
+            this.btnResult.UseVisualStyleBackColor = true;
+            this.btnResult.Click += new System.EventHandler(this.btnResult_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(674, 296);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 25);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "Кнопка Ленивца";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnResult);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnAcceptUserName);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtBoxUserName);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnFinish);
+            this.Controls.Add(this.comboBox7);
+            this.Controls.Add(this.comboBox6);
+            this.Controls.Add(this.comboBox5);
+            this.Controls.Add(this.comboBox4);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.RTB);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox RTB;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.ComboBox comboBox5;
+        private System.Windows.Forms.ComboBox comboBox6;
+        private System.Windows.Forms.ComboBox comboBox7;
+        private System.Windows.Forms.Button btnFinish;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.TextBox txtBoxUserName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label labelResult;
+        private System.Windows.Forms.Button btnAcceptUserName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnResult;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Victorina_WinForm_v1._1._2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public static int Count { get; set; }
+        public static int Goal { get; set; }
+        public static string UserName { get; set; }
+        public static string pathRtf = "QV.rtf";
+
+        public void AddFile()
+        {
+            string q = "Quest.rtf";
+            var newFile = File.Create(q);
+            StreamWriter sw = new StreamWriter(newFile);
+            sw.WriteLine("1.Единица измерения скорости.\r\na) кг b) литр c) км/ч.\r\n\n" +
+                "2.Какой по счету от Солнца является Земля?\r\na) Третья. b) Пятая c) Вторая\r\n\n" +
+                "3.Как называется прямая, ограниченная точками? \r\na) Луч b) Отрезок. c) Плоскость\r\n\n" +
+                "4.Сколько звуков в слове «конь»? \r\na) Четыре b) Три. c) Пять\r\n\n" +
+                "5.Выберите верное утверждение из перечисленных. \r\n" +
+                "a) Согласная буква не образует слог. b) Согласная образует слог c) Согласная в некоторых случаях образует слог\r\n\n" +
+                "6.Какой из этих цветов больше всего любят комары? \r\na) Желтый b) Синий. c) Красный\r\n\n" +
+                "7.Если смешать краску красного и синего цвета, получится… \r\na) Зеленый b) Фиолетовый. c) Черный");
+            sw.Close();
+            newFile.Close();
+        }
+
+        private void RTB_Click(object sender, EventArgs e)
+        {
+            AddFile();
+            try
+            {
+                RTB.LoadFile(pathRtf);
+                comboBox1.Enabled = true;
+                comboBox2.Enabled = true;
+                comboBox3.Enabled = true;
+                comboBox4.Enabled = true;
+                comboBox5.Enabled = true;
+                comboBox6.Enabled = true;
+                comboBox7.Enabled = true;
+                btnResult.Enabled = true;
+                btnAcceptUserName.Enabled = false;
+            }
+            catch
+            {
+                MessageBox.Show($"Не удалось открыт файл!Файл {pathRtf} не существует!" +
+                    $"\nСоздайте файл {pathRtf} с вопросамы, чтобы сработала программа!\n" +
+                    $"Если вам лень создавать файл {pathRtf} тогда нажмите на кнопку \"Кнопка Ленивца\"!");
+                button1.Visible = true;
+            }
+        }
+
+        private void btnFinish_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnAcceptUserName_Click(object sender, EventArgs e)
+        {
+            UserName = txtBoxUserName.Text;
+            if (txtBoxUserName.Text != " ")
+            {
+                RTB.Enabled = true;
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            txtBoxUserName.Enabled = true;
+            btnAcceptUserName.Enabled = true;
+            btnStart.Enabled = false;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string q = "Data.txt";
+                StreamWriter sw = File.AppendText(q);
+                sw.WriteLine(DateTime.Now);
+                sw.WriteLine($"Результаты пользователя {UserName}:\r\nПравильных ответов - {Count}\r\nВаша оценка - {Goal}\r\n");
+                sw.Close();
+                MessageBox.Show("Результаты сохранены в файл!");
+            }
+            catch
+            {
+                MessageBox.Show("Результаты несохранены в файл!");
+            }
+            btnSave.Enabled = false;
+        }
+
+        public void IsRigth()
+        {
+            if (comboBox1.Text == "c")
+            {
+                Count++;
+            }
+            if (comboBox2.Text == "a")
+            {
+                Count++;
+            }
+            if (comboBox3.Text == "b")
+            {
+                Count++;
+            }
+            if (comboBox4.Text == "b")
+            {
+                Count++;
+            }
+            if (comboBox5.Text == "a")
+            {
+                Count++;
+            }
+            if (comboBox6.Text == "b")
+            {
+                Count++;
+            }
+            if (comboBox7.Text == "b")
+            {
+                Count++;
+            }
+            btnFinish.Enabled = true;
+        }
+
+        public void Evaluations()
+        {
+            if (Count == 0)
+            {
+                Goal = 0;
+            }
+            if (Count == 1)
+            {
+                Goal = 1;
+            }
+            if (Count == 2)
+            {
+                Goal = 2;
+            }
+            if (Count == 3)
+            {
+                Goal = 3;
+            }
+            if (Count == 4 || Count == 5)
+            {
+                Goal = 4;
+            }
+            if (Count == 6 || Count == 7)
+            {
+                Goal = 5;
+            }
+            labelResult.Text = $"Результаты пользователя {UserName}:\r\nПравильных ответов - {Count}\r\nВаша оценка - {Goal}\r\n";
+        }
+
+        private void btnResult_Click(object sender, EventArgs e)
+        {
+            IsRigth();
+            Evaluations();
+            comboBox1.Enabled = false;
+            comboBox2.Enabled = false;
+            comboBox3.Enabled = false;
+            comboBox4.Enabled = false;
+            comboBox5.Enabled = false;
+            comboBox6.Enabled = false;
+            comboBox7.Enabled = false;
+            txtBoxUserName.Enabled = false;
+            btnResult.Enabled = false;
+            btnSave.Enabled = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            RTB.Text = "1.Единица измерения скорости.\r\na) кг b) литр c) км/ч.\r\n\r\n" +
+                "2.Какой по счету от Солнца является Земля?\r\na) Третья. b) Пятая c) Вторая\r\n\r\n" +
+                "3.Как называется прямая, ограниченная точками? \r\na) Луч b) Отрезок. c) Плоскость\r\n\r\n" +
+                "4.Сколько звуков в слове «конь»? \r\na) Четыре b) Три. c) Пять\r\n\r\n" +
+                "5.Выберите верное утверждение из перечисленных. \r\n" +
+                "a) Согласная буква не образует слог. b) Согласная образует слог c) Согласная в некоторых\r\n случаях образует слог\r\n\r\n" +
+                "6.Какой из этих цветов больше всего любят комары? \r\na) Желтый b) Синий. c) Красный\r\n\r\n" +
+                "7.Если смешать краску красного и синего цвета, получится… \r\na) Зеленый b) Фиолетовый. c) Черный";
+            comboBox1.Enabled = true;
+            comboBox2.Enabled = true;
+            comboBox3.Enabled = true;
+            comboBox4.Enabled = true;
+            comboBox5.Enabled = true;
+            comboBox6.Enabled = true;
+            comboBox7.Enabled = true;
+            btnResult.Enabled = true;
+        }
+    }
+}
+
+/// <summary>
+/// 
+/// 1.Единица измерения скорости.
+/// a) кг b) литр c) км/ч.
+/// 2.Какой по счету от Солнца является Земля?
+/// a) Третья.b) Пятая c) Вторая
+/// 3.Как называется прямая, ограниченная точками? 
+/// a) Луч b) Отрезок.c) Плоскость
+/// 4.Сколько звуков в слове «конь»? 
+/// a) Четыре b) Три.c) Пять
+/// 5.Выберите верное утверждение из перечисленных.
+/// a) Согласная буква не образует слог.b) Согласная образует слог c) Согласная в некоторых случаях образует слог
+/// 6.Какой из этих цветов больше всего любят комары? 
+/// a) Желтый b) Синий.c) Красный
+/// 7.Если смешать краску красного и синего цвета, получится… 
+/// a) Зеленый b) Фиолетовый.c) Черный
+/// </summary>
